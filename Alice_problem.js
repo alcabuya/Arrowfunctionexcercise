47e8bd4a47e31873e4f761ba532a322aff8581bf
Alberto Cabuya <alcabuyapre@gmail.com>
2021-09-25 13:27:55 +0200

    With converted number

--- Alice_problem.js
+++ Alice_problem.js
@@ -2,8 +2,8 @@ var seenum = document.getElementById("see");
 seenum.addEventListener('click',check_palyndrome_html,true);
 
 
-var base  = search_palindromenumbase(num);
-console.log(base);
+/*var base  = search_palindromenumbase(num);
+console.log(base);*/
 //subsequent divission to convert a number into a base
  //position 0 in the array
 // is the bit with the less significance  
@@ -48,26 +48,34 @@ function check_palindromebase(num,base){
     else{
         ispalindrom= true;
     }
-    return ispalindrom;
+    let result = [ispalindrom, basednum]
+    return result;
 }
 //search for the base where the number is palyndrome
 function search_palindromenumbase(num){
-    var palindrome_found = false;
+    let palindrome_found = [false, []];
     base = 1;
     
         do{
             base++;
             palindrome_found = check_palindromebase(num,base);                
-         }while(!palindrome_found);
-    
-    return base;
+         }while(!palindrome_found[0]);
+    //console.log(palindrome_found);
+    palindrome_found.splice(0,1);
+    var result = [base, palindrome_found]
+    return result;
 }
 //Call all the above functions to be implemented on the HTML page
 function check_palyndrome_html(){
     var numimp  =  document.getElementById("number");
     var num = numimp.value; 
-    var base =search_palindromenumbase(num);
-    var cont = document.getElementById("result");
-    cont.innerHTML= `<p> The base where the number is palyndrom is ${base}<p>`
+    let base =search_palindromenumbase(num);
+    var contbase = document.getElementById("baseresult");
+    var contnumm = document.getElementById("numberresult");
+    contbase.innerHTML= `<p> The base where the number is palindrom is ${base[0]}</p>`;
+    //delete elements from position 1 and stores it in different array
+    
+    let numresul = base.splice(1,1);
+    contnumm.innerHTML= `<p> Converted number = ${numresul}</p>`;
 
 }
